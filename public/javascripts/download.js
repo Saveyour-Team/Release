9ae7cc84ea172a463ca4a36c5ebf9cea505b0ec7
NateFuller <fatenuller@gmail.com>
2015-04-15 20:55:21 -0400

    more libs that should help figuring out how to make a zip file for download

---
+++ public/javascripts/download.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+
+	// figure out how to get a zip file of the "resources" file to download correctly
+	$("#download-btn").click(function() {
+		console.log("Button clicked!");
+	});
+
+});
+
