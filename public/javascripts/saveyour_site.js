a2a3767d2d36c18ec2eac2ed0773e90ce62002dd
NateFuller <fatenuller@gmail.com>
2015-05-02 21:30:54 -0400

    Added some more nav-bar hover styling functionality.

--- public/javascripts/saveyour_site.js
+++ public/javascripts/saveyour_site.js
@@ -3,17 +3,32 @@ $(document).ready(function() {
 	// handle nav-bar active class on page load
 	apply_active_class_on_load(document.location.pathname);
 
+	// hover feedback
+	apply_active_class_on_hover();
 		
 });
 
 function apply_active_class_on_load(pathname) {
 	if (pathname === '/') {
-		$('#download').addClass('active');
+		$('#download').addClass('active loaded');
 	} else if (pathname === '/docs') {
-		$('#docs').addClass('active');
+		$('#docs').addClass('active loaded');
 	} else if (pathname === '/plugins') {
-		$('#plugins').addClass('active');
+		$('#plugins').addClass('active loaded');
 	} else {
 		Console.log("Pathname: \"" + pathname + "\" not handled.");
 	}
+}
+
+function apply_active_class_on_hover() {
+
+	$('#main-nav li').mouseenter(function() {
+		if (!$(this).hasClass('loaded')) {
+			$(this).addClass('nav-hovered');
+			$('#main-nav li').mouseleave(function() {
+				$(this).removeClass('nav-hovered');
+			});
+		}
+	});
+	
 }
